0ac7c681e4f918880a926817d7e3f529cba10e26
mayangjie <mayangjie@kingmen.com.cn>
2020-04-07 17:19:53 +0800

    add migration report

---
+++ ReportGenerator/Migrations/Report/20200407091534_productionaddUnit-1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReportGenerator.Migrations.Report
+{
+    public partial class productionaddUnit1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Unit",
+                table: "Report",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Unit",
+                table: "Report");
+        }
+    }
+}
