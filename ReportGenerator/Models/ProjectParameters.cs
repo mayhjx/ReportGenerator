382d1b361679f78131fa1cfb0b07b2764be107a6
mayangjie <mayangjie@kingmen.com.cn>
2020-04-07 15:20:06 +0800

    修改ProjectParameters 有效数字位数的范围和最大允许误差的表现形式（百分数）

--- ReportGenerator/Models/ProjectParameters.cs
+++ ReportGenerator/Models/ProjectParameters.cs
@@ -15,16 +15,22 @@ namespace ReportGenerator.Models
         [Display(Name = "项目名称")]
         public string Name { get; set; }
 
+        [Required]
         [Display(Name = "最大允许误差")]
+        [DisplayFormat(DataFormatString = "{0:P0}")]
         public double ALE { get; set; }
 
+        [Required]
         [Display(Name = "医学决定水平一")]
         public double Xc1 { get; set; }
 
+        [Required]
         [Display(Name = "医学决定水平二")]
         public double Xc2 { get; set; }
 
+        [Required]
         [Display(Name = "结果有效数字位数")]
+        [Range(0,10,ErrorMessage ="有效数字位数范围：0~10")]
         public int SignificantDigits { get; set; }
 
     }
