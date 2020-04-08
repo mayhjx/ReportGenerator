c2623479270e6d8f2da823305458e874d10cb9a3
mayangjie <mayangjie@kingmen.com.cn>
2020-04-08 09:52:00 +0800

    增加查询搜索功能

--- ReportGenerator/Pages/QuantitativeReports/Index.cshtml.cs
+++ ReportGenerator/Pages/QuantitativeReports/Index.cshtml.cs
@@ -7,23 +7,50 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using ReportGenerator.Data;
 using ReportGenerator.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ReportGenerator.Pages.QuantitativeReports
 {
     public class IndexModel : PageModel
     {
-        private readonly ReportGenerator.Data.ReportContext _context;
+        private readonly ReportContext _context;
 
-        public IndexModel(ReportGenerator.Data.ReportContext context)
+        public IndexModel(ReportContext context)
         {
             _context = context;
         }
 
         public IList<Report> Report { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        public SelectList Status { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string ReportStatus { get; set; }
+
         public async Task OnGetAsync()
         {
-            Report = await _context.Report.ToListAsync();
+            // 查询报告状态和比对项目名称
+            IQueryable<string> StatusQuery = from r in _context.Report
+                                             orderby r.Status
+                                             select r.Status;
+
+            var reports = from p in _context.Report
+                          select p;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                reports = reports.Where(s => s.Item.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(ReportStatus))
+            {
+                reports = reports.Where(x => x.Status == ReportStatus);
+            }
+            Status = new SelectList(await StatusQuery.Distinct().ToListAsync());
+            Report = await reports.ToListAsync();
         }
     }
 }
