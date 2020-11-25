40663f2218623d7cef481feefc66f056a14da45f
mayangjie <mayangjie@kingmen.com.cn>
2020-11-25 10:43:20 +0800

    migration

--- ReportGenerator/Migrations/Report/20201125023703_AddFrontPageInformation.Designer.cs
+++ ReportGenerator/Migrations/Report/20201125023703_AddFrontPageInformation.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReportGenerator.Data;
 
 namespace ReportGenerator.Migrations.Report
 {
     [DbContext(typeof(ReportContext))]
-    partial class ReportContextModelSnapshot : ModelSnapshot
+    [Migration("20201125023703_AddFrontPageInformation")]
+    partial class AddFrontPageInformation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ReportGenerator/Migrations/Report/20201125023703_AddFrontPageInformation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReportGenerator.Migrations.Report
+{
+    public partial class AddFrontPageInformation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastInvestigationDate",
+                table: "Report",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProtocalID",
+                table: "Report",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Purpose",
+                table: "Report",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastInvestigationDate",
+                table: "Report");
+
+            migrationBuilder.DropColumn(
+                name: "ProtocalID",
+                table: "Report");
+
+            migrationBuilder.DropColumn(
+                name: "Purpose",
+                table: "Report");
+        }
+    }
+}
